d948bbad30cf97fcf4f013b55cf5d8e191e9d22a
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-20 19:29:59 +0100

    Done.

---
+++ app/helpers/products_helper.rb
@@ -0,0 +1,7 @@
+module ProductsHelper
+
+  def cart
+        session[:cart] ||= []
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'products#index'
+  #get '/', to: 'products#index'
+  post '/' => 'products#add'
+
+  resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
